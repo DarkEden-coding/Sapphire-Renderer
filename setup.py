25f22afad4c51967975511ff400e000a80fbbade
DarkEden-coding <darkedenc9@gmail.com>
2024-03-28 21:52:24 -0400

    change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 
 setup(
     name="sapphire-renderer",
-    version="0.0.17",
+    version="0.0.18",
     author="Dark_Eden",
     author_email="darkedenc9@gmail.com",
     description="A package to easily render simple 3D scenes",
