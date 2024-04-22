72fe5d2baf9550d1bc10f5fe92f57917d9d4d943
Dark-ind-Coding <darkedenc9@gmail.com>
2024-04-22 09:46:19 -0400

    fix surface object reference

--- sapphirerenderer/__init__.py
+++ sapphirerenderer/__init__.py
@@ -3,6 +3,5 @@ from .src.sapphirerenderer.utility_objects.particle_manager import ParticleManag
 
 from .src.sapphirerenderer.object_classes.base_object import Object
 from .src.sapphirerenderer.object_classes.flat_faces_object import FlatFacesObject
-from .src.sapphirerenderer.object_classes.surface_object import SurfaceObject
 from .src.sapphirerenderer.object_classes.text_object import TextObject
 from .src.sapphirerenderer.object_classes.wireframe_object import WireframeObject
