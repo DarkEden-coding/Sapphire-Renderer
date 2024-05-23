e50bbc615c0ca7b604ee964107fa069c37e0ef50
Dark-ind-Coding <darkedenc9@gmail.com>
2024-05-23 10:07:49 -0400

    change version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ install_requires = [
 
 setup(
     name="sapphire-renderer",
-    version="0.0.52",
+    version="0.0.53",
     author="Dark_Eden",
     author_email="darkedenc9@gmail.com",
     description="A package to easily render simple 3D scenes",
